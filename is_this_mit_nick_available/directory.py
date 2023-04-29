301c0c622873cf5dc66f80bbd9d2baf8bae18072
Kyle Anthony Williams <kyle.anthony.williams2@gmail.com>
2023-04-29 18:45:43 -0400

    Slighty improve search algo, forward server to network

--- is_this_mit_nick_available/directory.py
+++ is_this_mit_nick_available/directory.py
@@ -3,5 +3,5 @@ from urllib.request import urlopen
 from urllib.parse import quote
 
 def search(q: str) -> list:
-	with urlopen(f"https://tlepeopledir.mit.edu/q/{quote(q)}?_format=json") as res:
+	with urlopen(f"https://tlepeopledir.mit.edu/q/{quote(q + '@mit.edu')}?_format=json") as res:
 		return json.load(res).get("result", [])
--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 from is_this_mit_nick_available import app
 
-app.run(port=8000)
+app.run(host="0.0.0.0", port=8000)
